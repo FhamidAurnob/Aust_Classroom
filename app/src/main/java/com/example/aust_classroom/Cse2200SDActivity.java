691b8577df1c864dc3968337f8a5992121e5fef2
FhamidAurnob <fmaurnob0@gmail.com>
2019-10-09 23:15:53 +0600

    6th commit

---
+++ app/src/main/java/com/example/aust_classroom/Cse2200SDActivity.java
@@ -0,0 +1,14 @@
+package com.example.aust_classroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cse2200SDActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cse2200_sd);
+    }
+}
--- app/src/main/java/com/example/aust_classroom/CseCoursesActivity.java
+++ app/src/main/java/com/example/aust_classroom/CseCoursesActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -107,6 +108,18 @@ public class CseCoursesActivity extends AppCompatActivity {
                     });
             bmb.addBuilder(builder);
         }
+
+        softDevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (haveNetwork()){
+                    startActivity(new Intent(CseCoursesActivity.this, Cse2200SDActivity.class));
+                }
+                else if (!haveNetwork()){
+                    Toast.makeText(CseCoursesActivity.this, "Network connection unavailable!!\nCheck your 'internet' connection and try again", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
 
